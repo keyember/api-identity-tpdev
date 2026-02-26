424483e7d7a86d5f5d986e377d342a25d95066a0
keyember <dev.oudinallan@gmail.com>
2026-02-26 12:43:20 +0100

    design rework

--- identity.js
+++ identity.js
@@ -7,14 +7,12 @@ function generateIdentity() {
       const user = data.results[0];
       document.getElementById("lastname").textContent = user.name.last;
       document.getElementById("firstname").textContent = user.name.first;
-      ((document.getElementById("address").textContent =
-        user.location.street.number),
-        user.location.street.name,
-        user.location.city,
-        user.location.postcode,
-        user.location.state,
-        user.location.country);
-      document.getElementById("profilpicture").src = user.picture.medium;
+      document.getElementById("streetaddress").textContent =
+        user.location.street.name;
+      document.getElementById("postcodeaddress").textContent =
+        user.location.postcode;
+      document.getElementById("cityaddress").textContent = user.location.city;
+      document.getElementById("profilpicture").src = user.picture.large;
       document.getElementById("age").textContent = user.dob.age;
       document.getElementById("mail").textContent = user.email;
     })
